735f641426a25a66d76f31b34d34423ef88a7b30
KMarvin <kgmng920@gamil.com>
2022-06-09 17:16:29 +0900

    220609 commit

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -4,12 +4,24 @@ const registerAllApis = async (app, configs) => {
     // app.get('url', (req, res, next) => {})
     for (const apiName in configs) {
         const apiConfig = configs[apiName]
-        const { path: urlPath, method, handlerPath } = apiConfig
+        const { path: urlPath, method, handlerPath, handlerName } = apiConfig
 
         const apiModule = await import(
             path.join(__dirname, '../', '../', handlerPath)
         )
-        const {} = apiModule
+        const handlerFunction: (params: any, mysql: any) => Promise<any> =
+            apiModule[handlerName]
+
+        app[method](path, (req, res, next) => {
+            const params = req.body
+            const connection = req.mysqlConnection
+            handlerFunction(params, connection)
+                .then((result) => {
+                    res.json({ success: true, result: result })
+                })
+                .catch((e) => next(e))
+        })
+
         // app[method](path, )
     }
 }
